9e44b9704a35cb33e34c2a485a17a171329c2bc8
Sidpatchy <no-reply@sidpatchy.com>
2025-10-14 16:43:13 -0600

    fix: remove trailing slashes from API endpoints

--- src/main/java/com/sidpatchy/clairebot/API/APIUser.java
+++ src/main/java/com/sidpatchy/clairebot/API/APIUser.java
@@ -34,7 +34,7 @@ public class APIUser {
      */
     public void getUser() throws IOException {
         try {
-            user.loadFromURL(Main.getApiUser(), Main.getApiPassword(), Main.getApiPath() + "api/v1/user/" + userID);
+            user.loadFromURL(Main.getApiUser(), Main.getApiPassword(), Main.getApiPath() + "api/v1/user" + userID);
         }
         catch (Exception e) {
             if (createNewWithDefaults) {
@@ -94,7 +94,7 @@ public class APIUser {
                            List<Integer> pointsMessages,
                            List<Integer> pointsVoiceChat) throws IOException {
         POST post = new POST();
-        post.postToURL(Main.getApiPath() + "api/v1/user/", userConstructor(accentColour, language, pointsGuildID, pointsMessages, pointsVoiceChat));
+        post.postToURL(Main.getApiPath() + "api/v1/user", userConstructor(accentColour, language, pointsGuildID, pointsMessages, pointsVoiceChat));
     }
 
     public void createUserWithDefaults() {
@@ -126,7 +126,7 @@ public class APIUser {
         }
 
         PUT put = new PUT();
-        put.putToURL(Main.getApiPath() + "api/v1/user/" + userID,
+        put.putToURL(Main.getApiPath() + "api/v1/user" + userID,
                 userConstructor(accentColour, language, pointsGuildID, pointsMessages, pointsVoiceChat));
     }
 
@@ -171,7 +171,7 @@ public class APIUser {
 
     public void deleteUser() throws IOException {
         DELETE delete = new DELETE();
-        delete.deleteToURL(Main.getApiPath() + "api/v1/user/" + userID);
+        delete.deleteToURL(Main.getApiPath() + "api/v1/user" + userID);
     }
 
     /**
@@ -210,7 +210,7 @@ public class APIUser {
         URL url;
         InputStreamReader reader;
 
-        String link = Main.getApiPath() + "api/v1/user/";
+        String link = Main.getApiPath() + "api/v1/user";
         try {
             url = new URL(link);
             URLConnection uc = url.openConnection();
--- src/main/java/com/sidpatchy/clairebot/API/Guild.java
+++ src/main/java/com/sidpatchy/clairebot/API/Guild.java
@@ -29,7 +29,7 @@ public class Guild {
      */
     public void getGuild() throws IOException {
         try {
-            guild.loadFromURL(Main.getApiUser(), Main.getApiPassword(), Main.getApiPath() + "api/v1/guild/" + guildID);
+            guild.loadFromURL(Main.getApiUser(), Main.getApiPassword(), Main.getApiPath() + "api/v1/guild" + guildID);
         }
         catch (Exception e) {
             if (createNewWithDefaults) {
@@ -61,7 +61,7 @@ public class Guild {
                             String moderatorMessagesChannelID,
                             boolean enforceServerLanguage) throws IOException {
         POST post = new POST();
-        post.postToURL(Main.getApiPath() + "api/v1/guild/", guildConstructor(
+        post.postToURL(Main.getApiPath() + "api/v1/guild", guildConstructor(
                 requestsChannelID,
                 moderatorMessagesChannelID,
                 enforceServerLanguage
@@ -86,7 +86,7 @@ public class Guild {
                             String moderatorMessagesChannelID,
                             boolean enforceServerLanguage) throws IOException {
         PUT put = new PUT();
-        put.putToURL(Main.getApiPath() + "api/v1/guild/" + guildID, guildConstructor(
+        put.putToURL(Main.getApiPath() + "api/v1/guild" + guildID, guildConstructor(
                 requestsChannelID,
                 moderatorMessagesChannelID,
                 enforceServerLanguage
@@ -119,7 +119,7 @@ public class Guild {
 
     public void deleteGuild() throws IOException {
         DELETE delete = new DELETE();
-        delete.deleteToURL(Main.getApiPath() + "api/v1/guild/" + guildID);
+        delete.deleteToURL(Main.getApiPath() + "api/v1/guild" + guildID);
     }
 
     public String guildConstructor(String requestsChannelID,
@@ -141,7 +141,7 @@ public class Guild {
         URL url;
         InputStreamReader reader;
 
-        String link = Main.getApiPath() + "api/v1/guild/";
+        String link = Main.getApiPath() + "api/v1/guild";
         try {
             url = new URL(link);
             URLConnection uc = url.openConnection();
